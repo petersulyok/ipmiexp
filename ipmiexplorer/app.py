6c0b14a709503f69ac4801d2f6ff5e9f0992e6a0
petersulyok <peter@sulyok.net>
2025-02-18 17:45:57 +0100

    Empty application was created.

--- ipmiexplorer/app.py
+++ ipmiexplorer/app.py
@@ -2,23 +2,12 @@
 #   app.py (C) 2025, Peter Sulyok
 #   ipmiexplorer package: IpmiExplorerApp() class implementation.
 #
-from datetime import datetime
+
 from textual.app import App, ComposeResult
-from textual.widgets import Digits
+from textual.widgets import Header, Footer
 
 class IpmiExplorerApp(App):
-    CSS = """
-        Screen { align: center middle; }
-        Digits { width: auto; }
-        """
 
     def compose(self) -> ComposeResult:
-        yield Digits("")
-
-    def on_ready(self) -> None:
-        self.update_clock()
-        self.set_interval(1, self.update_clock)
-
-    def update_clock(self) -> None:
-        clock = datetime.now().time()
-        self.query_one(Digits).update(f"{clock:%T}")
+        yield Header(show_clock=True)
+        yield Footer()
