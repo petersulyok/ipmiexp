63501dd7f9e07f4f10171eb0197b2e6826695959
petersulyok <peter@sulyok.net>
2025-03-06 12:26:34 +0100

    Persistence added to the script.

--- test/ipmitool_emu.sh
+++ test/ipmitool_emu.sh
@@ -1,39 +1,112 @@
 #!/usr/bin/env bash
-# ipmitool emulation
+#
+#   ipmitool_emu.sh (C) 2025, Peter Sulyok
+#   This script will emulate the most important commands of `ipmitool`. It will persist fan mode and
+#   zone level information in the system temporary directory (e.g. /tmp/ipmiexp/...)
+#
+
+# Check the program temporary directory and create it if needed.
+tmp_dir=$(dirname "$(mktemp -u)")/ipmiexp
+if [ ! -d "$tmp_dir" ]; then
+    mkdir -p $tmp_dir
+    echo "1" >$tmp_dir/fan_mode
+    echo "64" >$tmp_dir/zone0
+    echo "64" >$tmp_dir/zone1
+    echo "64" >$tmp_dir/zone2
+    echo "64" >$tmp_dir/zone3
+    echo "64" >$tmp_dir/zone4
+    echo "64" >$tmp_dir/zone5
+    echo "64" >$tmp_dir/zone6
+    echo "64" >$tmp_dir/zone7
+fi
 
 # IPMI get fan mode (raw 0x30 0x45 0x00)
 if [[ $1 = "raw" && $2 = "0x30" && $3 = "0x45" && $4 = "0x00" ]] ; then
-    r=$((1 + (RANDOM % 4)))
+    file=$tmp_dir/fan_mode
+    if [ -f "$file" ]; then
+        r=$(<"$file")
+    else
+        r=$((1 + (RANDOM % 4)))
+    fi
     echo "$r"
     exit 0
 fi
 
-# IPMI set fan mode (raw 0x30 0x45 0x01)
+# IPMI set fan mode (raw 0x30 0x45 0x01 mode)
 if [[ $1 = "raw" && $2 = "0x30" && $3 = "0x45" && $4 = "0x01" ]] ; then
+    file=$tmp_dir/fan_mode
+    echo "$5" > "$file"
+    case "$5" in
+        "0")
+            # Standard mode: level 50% in all zones.
+            for i in $(seq 0 7); do
+                echo "32" > "$tmp_dir/zone$i"
+            done
+            ;;
+        "1")
+            # Full mode: level 100% in all zones.
+            for i in $(seq 0 7); do
+                echo "64" > "$tmp_dir/zone$i"
+            done
+            ;;
+        "2")
+            # Optimal mode: level 30% in all zones.
+            for i in $(seq 0 7); do
+                echo "1e" > "$tmp_dir/zone$i"
+            done
+            ;;
+        "3")
+            # PUE mode: level 30% in all zones.
+            for i in $(seq 0 7); do
+                echo "1e" > "$tmp_dir/zone$i"
+            done
+            ;;
+        "4")
+            # Heavy IO mode: CPU zone 30%, HD zone 75%.
+            echo "1e" > "$tmp_dir/zone0"
+            echo "4b" > "$tmp_dir/zone1"
+            echo "1" > "$tmp_dir/zone2"
+            echo "1" > "$tmp_dir/zone3"
+            echo "1" > "$tmp_dir/zone4"
+            echo "1" > "$tmp_dir/zone5"
+            echo "1" > "$tmp_dir/zone6"
+            echo "1" > "$tmp_dir/zone7"
+            ;;
+    esac
     exit 0
 fi
 
-# IPMI set fan level (raw 0x30 0x70 0x66 0x01)
+# IPMI set fan level (raw 0x30 0x70 0x66 0x01 zone level)
 if [[ $1 = "raw" && $2 = "0x30" && $3 = "0x70" && $4 = "0x66" && $5 = "0x01" ]] ; then
+    file=$tmp_dir/zone$6
+    printf "%x" "$7" > "$file"
     exit 0
 fi
 
-
-# IPMI get fan level (raw 0x30 0x70 0x66 0x00)
+# IPMI get fan level (raw 0x30 0x70 0x66 0x00 zone)
 if [[ $1 = "raw" && $2 = "0x30" && $3 = "0x70" && $4 = "0x66" && $5 = "0x00" ]] ; then
     if (( $6 < 8 )); then
-        echo ${RANDOM:0:2}
+        file=$tmp_dir/zone$6
+        if [ -f "$file" ]; then
+            r=$(<"$file")
+        else
+            r=$(printf "%x" "$((1 + (RANDOM % 100)))")
+        fi
+        echo "$r"
     else
-        echo "0"
+        echo "00"
     fi
     exit 0
 fi
 
 # IPMI read sensors.
 if [[ $1 = "-v" && $2 = "sdr" ]] ; then
-    curdir=$(dirname $0)
+    curdir=$(dirname "$0")
     cat "$curdir/ipmitool_sdr_v.txt"
     exit 0
 fi
 
+# Unknown command.
+exit 1
 
+# End.
