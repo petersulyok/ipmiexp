488e9ff0891ea6e92637ba234e78df596938a25c
petersulyok <peter@sulyok.net>
2025-03-06 12:24:55 +0100

    SetFanWindow class added

--- ipmiexp/modal.py
+++ ipmiexp/modal.py
@@ -1,11 +1,12 @@
 #
 #   ipmi.py (C) 2025, Peter Sulyok
-#   ipmiexp package: Ipmi() class implementation.
+#   ipmiexp package: ModalWindow() class implementation.
 #
 from textual.app import ComposeResult
 from textual.screen import ModalScreen
 from textual.containers import Container, Horizontal
-from textual.widgets import Label, Button
+from textual.widgets import Label, Button, RadioButton, RadioSet
+from ipmiexp import Ipmi
 
 class ModalWindow(ModalScreen):
     """A generic modal screen class."""
@@ -59,3 +60,77 @@ class ModalWindow(ModalScreen):
     def on_button_pressed(self, event):
         self.selected = event.button.id
         self.app.pop_screen()
+
+
+class SetFanModeWindow(ModalScreen):
+    """A modal pop-up window for setting new fan mode."""
+
+    DEFAULT_CSS = """
+    SetFanModeWindow {
+        align: center middle;
+    }
+
+    SetFanModeWindow > Container {
+        width: auto;
+        height: auto;
+        border: thick $background 80%;
+        background: $surface;
+    }
+
+    SetFanModeWindow > Container > Label {
+        width: 100%;
+        content-align-horizontal: center;
+        margin-top: 1;
+    }
+
+    SetFanModeWindow > Container > RadioSet {
+        width: 100%;
+        content-align-horizontal: center;
+        margin-top: 1;
+    }
+
+    SetFanModeWindow > Container > Horizontal {
+        width: auto;
+        height: auto;
+    }
+
+    SetFanModeWindow > Container > Horizontal > Button {
+        margin: 2 4;
+    }
+    """
+    mode: int
+
+    def __init__(self, mode: int):
+        self.mode = mode
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        with Container():
+            yield Label("Select new fan mode:")
+            with RadioSet(id="fan_modes", ):
+                yield RadioButton("Standard mode", value=bool(self.mode == Ipmi.STANDARD_MODE))
+                yield RadioButton("Full mode", value=bool(self.mode == Ipmi.FULL_MODE))
+                yield RadioButton("Optimal mode", value=bool(self.mode == Ipmi.OPTIMAL_MODE))
+                yield RadioButton("PUE mode", value=bool(self.mode == Ipmi.PUE_MODE))
+                yield RadioButton("Heavy IO mode", value=bool(self.mode == Ipmi.HEAVY_IO_MODE))
+            with Horizontal():
+                yield Button("Set mode", id="set_mode", variant="success")
+                yield Button("Cancel", id="_cancel", variant="error")
+
+    def on_button_pressed(self, event):
+        result = -1
+        if event.button.id == "set_mode":
+            buttons = list(self.query(RadioButton))
+            result = 0
+            for b in buttons:
+                if b.value:
+                    break
+                result += 1
+        self.dismiss(result)
+
+    def on_mount(self) -> None:
+        rs = self.query_one(RadioSet)
+        rs._selected = self.mode
+        rs.focus()
+
+# End.
